b20d3b92e3d6779dd8c47e1199f9a941526a4942
analyticgrit <analyticgrit@icloud.com>
2022-08-29 17:11:44 -0400

    completed Person2.java

--- src/main/java/edu/clayton/csit/antlab/person/Person2.java
+++ src/main/java/edu/clayton/csit/antlab/person/Person2.java
@@ -1,5 +1,9 @@
 package edu.clayton.csit.antlab.person;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
@@ -30,8 +34,18 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
+		List<String> inputs = new ArrayList<>(List.of(input.split("")));
+		String outputs = "";
+//		System.out.printf(inputs.toString());
+		Random rand = new Random();
+		while(!inputs.isEmpty()){
+			int val = rand.nextInt(inputs.size());
+			outputs += inputs.get(val);
+			inputs.remove(val);
+		}
+//		System.out.printf("\nRandom String: %s\n",outputs);
 	  //Person 2 put your implementation here
-	  return null;
+	  return outputs;
 	}
 	/**
 	 * Return a string rep of this object
@@ -43,5 +57,11 @@ public class Person2 {
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
+	}
+
+	public static void main(String[] args) {
+		Person2 p = new Person2("AnnMarie");
+		p.calc("AnnMarie");
+		System.out.printf("%s",p.toString("Whitley"));
 	}
 }
